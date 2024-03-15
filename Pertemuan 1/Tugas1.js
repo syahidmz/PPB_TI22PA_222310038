db8f9fead583c41c7f28e03e1f30f36260b26e28
syahidmz <113004243+syahidmz@users.noreply.github.com>
2024-03-16 04:31:53 +0700

    Add files via upload

---
+++ Pertemuan 1/Tugas1.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import {View, StyleSheet, Text, Image} from 'react-native';
+
+class Tugas1 extends Component {
+    render() {
+        return (
+            <View>
+               <Text>My Profile!</Text>
+                <Text>NAMA                         : Syahid Muhammad Zubeir</Text>
+                <Text>KELAS                        : TI-22-PA </Text>
+                <Text>NPM                          : 222310038</Text>
+                <Text>NAMA PANGGILAN : Daeron</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({})
+
+export default Tugas1;
